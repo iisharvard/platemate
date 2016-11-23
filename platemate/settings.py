10c9fc101eeb333b88b6c73c42033d8356220f77
Tom Smyth <tom@sassafras.coop>
2016-11-23 16:05:52 -0500

    Upgraded to Django 1.8

--- platemate/settings.py
+++ platemate/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware'
 )
 
 ROOT_URLCONF = 'urls'
