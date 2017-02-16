7f8be2c76f11f4d992ac53e145f8b547044b1271
Devney Hamilton <devney.hamilton@atomicobject.com>
2017-02-16 13:26:12 -0500

    Delete __str__ overrides for BoxGroup and IngredientList models. These cause infinite depth errors when __dict__ is called on these models in the downcast save method.

--- platemate/food/models/common.py
+++ platemate/food/models/common.py
@@ -171,9 +171,6 @@ class BoxGroup(SmartModel):
     def submission(self):
         return self.photo.submission
 
-    def __str__(self):
-        return str([str(b) for b in self.boxes.all()])
-
     @staticmethod
     def from_json(json_str, photo=None):
         new_group = BoxGroup()
@@ -428,9 +425,6 @@ class IngredientList(SmartModel):
 
     def __eq__(self,other):
         return [i.food.pk for i in self.ingredients.all()] == [i.food.pk for i in other.ingredients.all()]
-
-    def __str__(self):
-        return str([str(i) for i in self.ingredients.all()])
 
     @staticmethod
     def from_json(json_str, box=None):
