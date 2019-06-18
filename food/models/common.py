9b57230e3795d1a5b5b187c707e6e1735b10f5ed
Yishen Miao <mys721tx@gmail.com>
2019-06-18 13:11:55 -0400

    Fixing schema
    Fixes schema.

--- food/models/common.py
+++ food/models/common.py
@@ -107,6 +107,7 @@ class Submission(SmartModel):
 
 class Photo(SmartModel):
     photo_url = URLField(verbose_name="Photo URL")
+    has_food = BooleanField(default=False)
 
     def __str__(self):
         return self.photo_url
