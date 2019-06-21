30b2b3e81ce3f0a499ccaa965431bdc87e8722be
Yishen Miao <mys721tx@gmail.com>
2019-06-21 14:27:03 -0400

    Switching to Postgres
    Switches to Postgres.

--- settings.py
+++ settings.py
@@ -23,15 +23,15 @@ URL_PATH = os.getenv("BASE_URL", "http://localhost")
 DATABASES = {
     'default': {
         # Engines include '.postgresql_psycopg2', '.postgresql', '.mysql', '.sqlite3' or '.oracle'.
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
-        #'USER': '',                      # Not used with sqlite3.
-        #'PASSWORD': '',                  # Not used with sqlite3.
-        #'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        #'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        'OPTIONS': {
-            'timeout': 20
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'platemate',
+        'USER': os.getenv("DB_USER", "platemate"),                      # Not used with sqlite3.
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),                  # Not used with sqlite3.
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.getenv("DB_PORT", "5432"),                      # Set to empty string for default. Not used with sqlite3.
+        # 'OPTIONS': {
+        #     'timeout': 20
+        # }
     }
 }
 
