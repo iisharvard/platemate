087e59651e5e42063cd01a3910b895d7c679bdbd
Kliment Mamykin <kmamyk@gmail.com>
2020-01-19 17:05:00 -0500

    Updated Qualifications request formatting mturk 2

--- management/qualifications.py
+++ management/qualifications.py
@@ -1,13 +1,37 @@
-from boto.mturk import qualification
-
 # QUALIFICATIONS
+# The magic constants for QualificationTypeId are documented in
+# https://docs.aws.amazon.com/AWSMechTurk/latest/AWSMturkAPI/ApiReference_QualificationRequirementDataStructureArticle.html#ApiReference_QualificationType-IDs
 # --------------
+
+
 def min_approval(score):
-    return qualification.PercentAssignmentsApprovedRequirement('GreaterThanOrEqualTo', score).get_as_params()
+    qualification = {
+        'QualificationTypeId': '000000000000000000L0',
+        'Comparator': 'GreaterThanOrEqualTo',
+        'IntegerValues': [score],
+        'ActionsGuarded': 'Accept',
+    }
+    return qualification
+
 
 def min_completed(hits):
-    return qualification.NumberHitsApprovedRequirement('GreaterThanOrEqualTo', hits).get_as_params()
+    qualification = {
+        'QualificationTypeId': '00000000000000000040',
+        'Comparator': 'GreaterThanOrEqualTo',
+        'IntegerValues': [hits],
+        'ActionsGuarded': 'Accept',
+    }
+    return qualification
+
 
 def locale(country_code):
     # http://www.iso.org/iso/country_codes/iso_3166_code_lists/english_country_names_and_code_elements.htm
-    return qualification.LocaleRequirement('EqualTo', country_code, True).get_as_params()
+    qualification = {
+        'QualificationTypeId': '00000000000000000071',
+        'Comparator': 'EqualTo',
+        'LocaleValues':[{
+            'Country': country_code
+        }],
+        'ActionsGuarded': 'Accept',
+    }
+    return qualification
