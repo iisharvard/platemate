7bf98730730e1f5a2a8fc512d4051b30c9079b13
Tom Smyth <tom@sassafras.coop>
2020-07-07 14:57:17 -0400

    Add log timestamp

--- settings.py
+++ settings.py
@@ -148,9 +148,9 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'verbose': {
+        'stamped': {
             'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'datefmt' : "%Y-%m-%d %H:%M:%S"
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -160,16 +160,19 @@ LOGGING = {
         'verbose': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_PATH, 'log/verbose.log')
+            'filename': os.path.join(BASE_PATH, 'log/verbose.log'),
+            'formatter': 'stamped'
         },
         'concise': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_PATH, 'log/concise.log')
+            'filename': os.path.join(BASE_PATH, 'log/concise.log'),
+            'formatter': 'stamped'
         },
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'stamped'
         },
     },
     'loggers': {
