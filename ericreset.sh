8e382507ebb2eda26e1e499e731de4121cffa14e
Tom Smyth <tom@sassafras.coop>
2020-07-24 11:24:12 -0400

    Remove old default user and ericreset script

--- ericreset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm food/fixtures/initial_data.json
-python manage.py dumpdata food.Food food.FoodSearchResults food.FoodSearchResult food.Serving --indent 4 --format=json > food/fixtures/initial_data.json
-rm db
-python manage.py syncdb
-python test_data.py
