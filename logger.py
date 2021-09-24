7ffaceab89f5844c0d02f0877242564947c9e6fb
Tom Smyth <tom@sassafras.coop>
2021-09-24 12:06:18 -0400

    Refactor log levels

--- logger.py
+++ logger.py
@@ -9,14 +9,13 @@ import pprint
 # DEBUG 10
 # NOTSET 0
 
-LOGGING_LEVEL = 10
-FOOD_CONTROL = 40
-MANAGER_CONTROL = 30
-TURK_CONTROL = 25
-LOOP_CONTROL = 20
-LOOP_SETUP = 30
-CONTENT_TYPE_WARNING = 15
-TURK_WARNING = 50
+MIN_LEVEL = 10
+
+FOOD_CONTROL = 20
+LOOP_CONTROL = 10
+LOOP_SETUP = 20
+MANAGER_CONTROL = 20
+TURK_CONTROL = 20
 
 logger = logging.getLogger('django')
 
@@ -24,7 +23,7 @@ content_type_logger = logging.getLogger('content_type_logger')
 content_type_logger.setLevel(logging.INFO)
 
 def log(message, level, *args):
-    if level >= LOGGING_LEVEL:
+    if level >= MIN_LEVEL:
         logger.log(level, message)
         for arg in args:
             logger.log(level, pprint.pformat(arg))
