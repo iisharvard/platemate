3398e53afc187481d26362cfa172b83c478e3678
Tom Smyth <tom@sassafras.coop>
2021-09-24 12:30:07 -0400

    Add error handling wrapper for processor

--- uploads.py
+++ uploads.py
@@ -1,13 +1,17 @@
 from management.run import run
 from food.models import chiefs
+from logger import *
 import sys
 
 # Example usage: "python uploads.py sandbox"
 operation = sys.argv[1]
 mode = sys.argv[-1]
 
-run(
-    chief_module=chiefs.from_uploads,
-    args={'sandbox': (mode != 'real')},
-    operation=operation
-)
+try:
+    run(
+        chief_module=chiefs.from_uploads,
+        args={'sandbox': (mode != 'real')},
+        operation=operation
+    )
+except:
+    logger.exception("Fatal error in processor")
